cdc53df04b8f90996956b1427d36cfa86bfca0ff
chohyunki <wdlcho@wedatalab.com>
2020-10-07 14:17:46 +0900

    초기 배포 Solidity 코드

---
+++ Mytoken.sol
@@ -0,0 +1,8 @@
+pragma solidity >=0.4.0 <0.7.0;
+
+import "./Context.sol";
+import "./Ownable.sol";
+
+contract MyToken is Context, Ownable{
+    
+}
---
+++ Ownable.sol
@@ -0,0 +1,25 @@
+pragma solidity >=0.4.0 <0.7.0;
+
+import "./Context.sol";
+
+contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    constructor () internal {
+        address msgSender = _msgSender();
+        _owner = msgSender;
+        emit OwnershipTransferred(address(0), msgSender);
+    }
+
+
+    function owner() public view returns (address) {
+        return _owner;
+    }
+
+
+    modifier onlyOwner() {
+        require(_owner == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
